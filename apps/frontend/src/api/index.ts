64c0b42afc1f466a2e5382ca355f0d82caebecfe
piemekanika@hotmail.com <piemekanika@hotmail.com>
2022-09-08 22:08:53 +0700

    made some shitty garbage

--- apps/frontend/src/api/index.ts
+++ apps/frontend/src/api/index.ts
@@ -1,13 +1,53 @@
 import { ShopList } from '../dto';
+import { QueryObject, withQuery } from 'ufo';
 
-const baseUrl = 'http://localhost:3000/'
-const getUrl = (endpoint: string) => baseUrl + endpoint
+const baseUrl = 'http://localhost:3000/';
+const buildFullUrl = (endpoint: string) => baseUrl + endpoint;
 
-const api = {
-
-    getShops(): Promise<ShopList> {
-        return fetch(getUrl('shop-list')).then(r => r.json())
-    },
+interface Api {
+    [ name: string ]: (params?: any) => Promise<any>;
 }
 
-export { api }
+const api: Api = {};
+
+api.getShops = createApiMethodWithQueryParams<undefined, ShopList>({
+    url: 'shop-list',
+    method: 'get',
+});
+
+interface ApiMethodParams {
+    method: string;
+    url: string;
+}
+
+function createApiMethodWithQueryParams<RequestType extends QueryObject | undefined, ResponseType>(options: ApiMethodParams) {
+    return (params: RequestType): Promise<ResponseType> => {
+        const urlWithQuery = params
+            ? withQuery(options.url, params)
+            : options.url;
+
+        const url = buildFullUrl(urlWithQuery);
+
+        return fetch(url, { method: options.method }).then(r => r.json());
+    };
+}
+
+function createApiMethodWithBodyJson<RequestType, ResponseType>(options: ApiMethodParams) {
+    return (params: RequestType): Promise<ResponseType> => {
+
+        const body = JSON.stringify(params);
+
+        const url = buildFullUrl(options.url);
+
+        return fetch(url, {
+            method: options.method,
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            body,
+        })
+            .then(r => r.json());
+    };
+};
+
+export { api };
