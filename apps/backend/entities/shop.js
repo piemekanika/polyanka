5b1a441526776e5b5459aee25bd188196eb7ee2a
piemekanika@hotmail.com <piemekanika@hotmail.com>
2022-09-14 20:37:48 +0700

    added sorting by id

--- apps/backend/entities/shop.js
+++ apps/backend/entities/shop.js
@@ -5,7 +5,7 @@ module.exports = {
         return pool.query(`select bodyjson from shops where bodyjson->>'id' = ${id}`);
     },
     getShops() {
-        return pool.query('select bodyjson from shops')
+        return pool.query('select bodyjson from shops order by cast(bodyjson->>\'id\' as int)')
             .then(r => r.rows.map(e => e.bodyjson))
     },
 };
