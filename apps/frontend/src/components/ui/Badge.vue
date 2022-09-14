9cfbbf3eb03b99b1d2c1a5cdde45f5ac23bf8be5
piemekanika@hotmail.com <piemekanika@hotmail.com>
2022-09-14 20:57:01 +0700

    made badges do colors

--- apps/frontend/src/components/ui/Badge.vue
+++ apps/frontend/src/components/ui/Badge.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 const props = defineProps<{
     label: string
     color: string
-}>()
+}>();
+
+const classList = computed(() => {
+    return `badge badge--${props.color}`;
+});
 </script>
 
 <template>
-    <div class="border rounded-full font-bold w-[fit-content] py-1 px-4">
+    <div
+        class="rounded-full font-bold w-[fit-content] py-1 px-4"
+        :class="classList"
+    >
         {{ props.label }}
     </div>
 </template>
+
+<style lang="scss">
+.badge {
+    &--green {
+        background-color: #B3E1BA;
+    }
+
+    &--red {
+        background-color: #E1B3B3;
+    }
+}
+</style>
